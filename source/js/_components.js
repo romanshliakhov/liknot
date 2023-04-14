ab41e9dbc80203500a66e7cc0d835bcb23e5aa76
Roman <romanshliakhov@gmail.com>
2023-04-14 12:23:36 +0300

    starting project

---
+++ source/js/_components.js
@@ -0,0 +1,23 @@
+// import './components/sliders.js';
+// import './components/select.js';
+import './components/burger.js';
+import './components/accordion.js';
+// import './components/modals.js';
+import './components/replaceEl.js';
+import './components/links.js';
+import './components/hideSticky.js';
+import './components/lightGallery.js';
+// import './components/phoneMask.js';
+// import './components/tabs.js';
+// import './components/side-panel.js';
+// import './components/closeAlert.js';
+// import './components/choices.js';
+// import './components/showPassword.js';
+
+
+
+
+
+
+
+
