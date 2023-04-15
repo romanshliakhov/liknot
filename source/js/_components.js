51babf4e747c1a3e6c518e898f0a76e0be0063ad
Roman <romanshliakhov@gmail.com>
2023-04-15 22:35:31 +0300

    add partners section

--- source/js/_components.js
+++ source/js/_components.js
@@ -1,11 +1,11 @@
-// import './components/sliders.js';
+import './components/sliders.js';
 // import './components/select.js';
 import './components/burger.js';
 import './components/accordion.js';
 // import './components/modals.js';
 import './components/replaceEl.js';
 import './components/links.js';
-import './components/hideSticky.js';
+// import './components/hideSticky.js';
 import './components/lightGallery.js';
 // import './components/phoneMask.js';
 import './components/tabs.js';
