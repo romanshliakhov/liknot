c96a109746dfb0daaab9b461518f10311cd871b6
Roman <romanshliakhov@gmail.com>
2023-04-19 03:27:26 +0300

    fix links scroll mobile

--- source/js/components/burger.js
+++ source/js/components/burger.js
@@ -3,12 +3,20 @@ let menuBtn = document.querySelector('.burger');
 let menu = document.querySelector('.mobile');
 let disableScroll = document.querySelector('body');
 
-if (menuBtn) {
-  menuBtn.addEventListener('click', function(){
-    menuBtn.classList.toggle('active');
+if (menu && menuBtn) {
+  menuBtn.addEventListener('click', () => {
     menu.classList.toggle('active');
+    menuBtn.classList.toggle('active');
     disableScroll.classList.toggle('body-scroll');
-  });
+  })
+
+  menu.querySelectorAll('.mobile a').forEach(link => {
+    link.addEventListener('click', () => {
+      menu.classList.remove('active');
+      menuBtn.classList.remove('active');
+      disableScroll.classList.remove('body-scroll');
+    })
+  })
 }
 
 
--- source/js/components/smoothScroll.js
+++ source/js/components/smoothScroll.js
@@ -1,13 +1,42 @@
-const anchorButtons = document.querySelectorAll('.nav__link');
+// const anchorButtons = document.querySelectorAll('.nav__link');
 
-for(const anchorButton of anchorButtons) {
-  anchorButton.addEventListener('click', function(e){
-    e.preventDefault();
+// for(const anchorButton of anchorButtons) {
+//   anchorButton.addEventListener('click', function(e){
+//     e.preventDefault();
 
-    if (this.classList.contains('nav__link')) {
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
+//     if (this.classList.contains('nav__link')) {
+//       document.querySelector(this.getAttribute('href')).scrollIntoView({
+//         behavior: 'smooth'
+//       });
+//     }
+//   });
+// }
+
+
+
+const anchorButtons = document.querySelectorAll('.nav__list');
+const header = document.querySelector('header');
+
+let menuBtn = document.querySelector('.burger');
+let menu = document.querySelector('.mobile');
+let disableScroll = document.querySelector('body');
+
+window.addEventListener('DOMContentLoaded', () => {
+  anchorButtons.forEach(function(item){
+    item.addEventListener('click', function(e){
+      let clickValue = e.target.getAttribute('href');
+
+      e.preventDefault();
+
+      menu.classList.remove('active');
+      menuBtn.classList.remove('active');
+      disableScroll.classList.remove('body-scroll');
+
+
+      window.scrollTo({
+        top: document.getElementById(`${clickValue.replace('#', '')}`).offsetTop - (header.clientHeight * 1.5),
         behavior: 'smooth'
-      });
-    }
-  });
-}
+      })
+    });
+  })
+})
