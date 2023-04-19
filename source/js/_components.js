6815fab5c1ca46eb9b940f8b703e85a5c5b4bdf5
Roman <romanshliakhov@gmail.com>
2023-04-19 08:51:13 +0300

    add tooltip

--- source/js/_components.js
+++ source/js/_components.js
@@ -1,7 +1,7 @@
+import './components/burger.js';
 import './components/animation.js';
 import './components/sliders.js';
 // import './components/select.js';
-import './components/burger.js';
 import './components/accordion.js';
 import './components/modals.js';
 import './components/replaceEl.js';
