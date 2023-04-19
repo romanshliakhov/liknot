df90ea77c78998ae32685a63b6a34f6c87ad811c
Roman <romanshliakhov@gmail.com>
2023-04-19 03:34:42 +0300

    fix scroll to block

--- source/js/components/smoothScroll.js
+++ source/js/components/smoothScroll.js
@@ -1,19 +1,3 @@
-// const anchorButtons = document.querySelectorAll('.nav__link');
-
-// for(const anchorButton of anchorButtons) {
-//   anchorButton.addEventListener('click', function(e){
-//     e.preventDefault();
-
-//     if (this.classList.contains('nav__link')) {
-//       document.querySelector(this.getAttribute('href')).scrollIntoView({
-//         behavior: 'smooth'
-//       });
-//     }
-//   });
-// }
-
-
-
 const anchorButtons = document.querySelectorAll('.nav__list');
 const header = document.querySelector('header');
 
